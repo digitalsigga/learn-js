c7b6cd80f4536214dc8ba1079d86cadd2d905772
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2023-12-20 10:09:06 +0000

    feat : added a div in a pink container

--- DOM/script.js
+++ DOM/script.js
@@ -1,7 +1,34 @@
 const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+const redText = document.createElement('p');
+redText.classList.add('content');
+redText.textContent = 'Hey I am red';
+redText.style.color = "red";
 
-container.appendChild(content);
+const blueText = document.createElement('h3');
+blueText.classList.add('blueText');
+blueText.textContent ="I'm a blue h3"; 
+blueText.style.color = "blue";
+
+const blackBorder = document.createElement('div');
+blackBorder.classList.add('blackBorder');
+blackBorder.style.backgroundColor = "pink";
+blackBorder.style.borderColor= "black";
+
+const iAm= document.createElement('h1');
+iAm.classList.add("iAm");
+iAm.textContent = "I'm in a div";
+
+const meToo = document.createElement('p');
+meToo.classList.add("meToo");
+meToo.textContent = "ME TOO!";
+
+
+
+
+container.appendChild(redText);
+container.appendChild(blueText);
+
+blackBorder.appendChild(iAm);
+blackBorder.appendChild(meToo);
+container.appendChild(blackBorder);
